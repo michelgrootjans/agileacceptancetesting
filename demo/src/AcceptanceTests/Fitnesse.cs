3d91db344273e8da159b6800e7c00336539904cb
michel.grootjans <michel.grootjans@3b5158dc-0da6-11de-a42d-873b86d837ff>
2010-01-30 16:37:25 +0000

    Fitnesse.Init is now run in fixture.SetUp. No need to reference it in the testclasses.

--- demo/src/AcceptanceTests/Fitnesse.cs
+++ demo/src/AcceptanceTests/Fitnesse.cs
@@ -1,21 +1,15 @@
 using System.Collections.Generic;
-using System.Threading;
 using Snacks_R_Us.Domain;
 using Snacks_R_Us.Domain.Services;
 
 namespace Snacks_R_Us.AcceptanceTests
 {
-    internal static class Fitnesse
+    public class Fitnesse
     {
-        internal static void Init()
+        public void Init()
         {
             Current.Context = new StaticContext(new Dictionary<string, object>());
             ApplicationStartup.Run();
-        }
-
-        internal static void WaitASecond()
-        {
-            Thread.Sleep(5000);
         }
     }
 }
--- demo/src/AcceptanceTests/Given.cs
+++ demo/src/AcceptanceTests/Given.cs
@@ -3,10 +3,6 @@ using Snacks_R_Us.Domain;
 namespace Snacks_R_Us.AcceptanceTests
 {
     public class Given     {
-        public Given()
-        {
-            Fitnesse.Init();
-        }
 
         public void AUserWithCredits(string userName, double credit)
         {
